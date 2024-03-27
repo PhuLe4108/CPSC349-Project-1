e2d395b99bb3e8adaa074b6878b61696ccd8e835
Daniel Le <PhuLe4108@csu.fullerton.edu>
2024-03-27 12:54:51 -0700

    Added a JS file

---
+++ script.js
@@ -0,0 +1,7 @@
+//Homepage script
+function homeButtonClicked() {
+    let content = document.getElementById("home-button").textContent
+    const result = `${content} button clicked`;
+    alert(result);
+}
+//
