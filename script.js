fe97781c5f5b75b54ee47530707beb3f57d7449e
Daniel Le <PhuLe4108@csu.fullerton.edu>
2024-03-31 01:14:43 -0700

    added quiz 2 and able to show quizzes on homepage.html

--- script.js
+++
@@ -1,7 +0,0 @@
-//Homepage script
-function homeButtonClicked() {
-    let content = document.getElementById("home-button").textContent
-    const result = `${content} button clicked`;
-    alert(result);
-}
-//
