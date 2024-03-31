28903adc9714b5cb42a51f325b9ba216784d9fa2
gabbeans <hello.gabbyy@outlook.com>
2024-03-31 15:02:51 -0700

    Score Keeper

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function ScoreButtonClicked() {
   const result = `${content} button clicked`;
   alert(result);
 }
+
+var quiz1Score;
+var quiz2Score;
+var quiz3Score;
+
+function quiz1Score() {
+  quiz1Score = document.getElementById("quiz1Score").value;
+  alert("Quiz 1 Score: " + quiz1Score);
+}
+function quiz2Score() {
+  quiz2Score = document.getElementById("quiz2Score").value;
+  alert("Quiz 2 Score: " + quiz2Score);
+}
+function quiz3Score() {
+  quiz3Score = document.getElementById("quiz3Score").value;
+  alert("Quiz 3 Score: " + quiz3Score);
+}
