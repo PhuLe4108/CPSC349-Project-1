2ea09d88601f9ce955e35a9a35441c1765532208
gabbeans <hello.gabbyy@outlook.com>
2024-03-31 14:51:18 -0700

    merged my quiz questions with original homepage

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 //Homepage script
 function homeButtonClicked() {
-    let content = document.getElementById("home-button").textContent
-    const result = `${content} button clicked`;
-    alert(result);
+  let content = document.getElementById("home-button").textContent;
+  const result = `${content} button clicked`;
+  alert(result);
 }
-//
+//Score tracker
+function ScoreButtonClicked() {
+  let content = document.getElementById("score-button").textContent;
+  const result = `${content} button clicked`;
+  alert(result);
+}
